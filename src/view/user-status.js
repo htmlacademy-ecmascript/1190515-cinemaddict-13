35800bfcd29ecf2121da7af0cfa934a364d3ccda
j3n4r3v <jene4ka4reva@gmail.com>
2021-01-06 00:36:39 +0200

    delete rated and commented

--- src/view/user-status.js
+++ src/view/user-status.js
@@ -1,8 +1,10 @@
+
+import {generateProfile} from "./profile";
 export const createUserStatusTemplate = (rank = ``) => {
   return (
     `<section class="header__profile profile">
       <p class="profile__rating">${rank}</p>
-      <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
+      <img class="profile__avatar" src="${generateProfile}" alt="Avatar" width="35" height="35">
     </section>`
   );
 };
