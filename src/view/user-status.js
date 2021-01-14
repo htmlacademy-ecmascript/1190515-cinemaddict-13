4408ea0d21e6895478fba6a4d9b43f2eb2dcfb9a
j3n4r3v <jene4ka4reva@gmail.com>
2021-01-14 12:49:44 +0200

    fix changes

--- src/view/user-status.js
+++ src/view/user-status.js
@@ -4,7 +4,7 @@ export const createUserStatusTemplate = (rank) => {
   return (
     `<section class="header__profile profile">
       <p class="profile__rating">${rank}</p>
-      <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
+      <img class="profile__avatar" src="Profile" alt="Avatar" width="35" height="35">
     </section>`
   );
 };
