bc0e37f693bd03146db03e9261312ece14b5976d
j3n4r3v <jene4ka4reva@gmail.com>
2021-01-15 11:37:57 +0200

    rewiew

--- src/main.js
+++ src/main.js
@@ -80,18 +80,19 @@ const renderAdditionBlocks = (filmContainerElement, filmsSortingByRating, filmsS
   for (let i = 0; i < FILM_COUNT_ADDITION; i++) {
     render(filmContainerElement, new AdditionBlockView().getElement(), POSITION.BEFOREEND);
     const extraContainerElements = filmContainerElement.querySelectorAll(`.films-list--extra`);
-    const additionContainerElement = extraContainerElements[extraContainerElements.length - 1];
-    const cards = ADDITION_CONTAINER_TITLES[i] === `Top rated` ? filmsSortingByRating : filmsSortingByCommentsCount;
+    const firstextra = extraContainerElements[0];
+    const secondextra = extraContainerElements[1];
+    const cards = ADDITION_CONTAINER_TITLES[i] === ADDITION_CONTAINER_TITLES[0] ? filmsSortingByRating : filmsSortingByCommentsCount;
 
     if (ADDITION_CONTAINER_TITLES[i] === `Top rated` && cards[0].rating > 0) {
-      additionContainerElement.querySelector(`.films-list__title`).textContent = ADDITION_CONTAINER_TITLES[i];
+      firstextra.querySelector(`.films-list__title`).textContent = ADDITION_CONTAINER_TITLES[i];
       cards.slice(0, FILM_COUNT_ADDITION).forEach((card) => {
-        renderFilm(additionContainerElement.querySelector(`.films-list__container`), card, POSITION.BEFOREEND);
+        renderFilm(firstextra.querySelector(`.films-list__container`), card, POSITION.BEFOREEND);
       });
     } else if (ADDITION_CONTAINER_TITLES[i] === `Most commented` && cards[0].comments.length > 0) {
-      additionContainerElement.querySelector(`.films-list__title`).textContent = ADDITION_CONTAINER_TITLES[i];
+      secondextra.querySelector(`.films-list__title`).textContent = ADDITION_CONTAINER_TITLES[i];
       cards.slice(0, FILM_COUNT_ADDITION).forEach((card) => {
-        renderFilm(additionContainerElement.querySelector(`.films-list__container`), card, POSITION.BEFOREEND);
+        renderFilm(secondextra.querySelector(`.films-list__container`), card, POSITION.BEFOREEND);
       });
     }
   }
