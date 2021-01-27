ca248e321eb50322ffeb1e760de010b4620fdd9c
j3n4r3v <jene4ka4reva@gmail.com>
2021-01-27 09:26:55 +0200

    fix repeat

--- src/utils/render.js
+++ src/utils/render.js
@@ -1,64 +1,3 @@
-const POSITION = {
-  AFTERBEGIN: `afterbegin`,
-  AFTEREND: `afterend`,
-  BEFOREEND: `beforeend`
-};
-
-const createElement = (template) => {
-  const newElement = document.createElement(`div`);
-  newElement.innerHTML = template;
-  return newElement.firstChild;
-};
-
-const render = (container, component, position) => {
-  const element = component.getElement();
-  switch (position) {
-    case POSITION.AFTERBEGIN:
-      container.prepend(element);
-      break;
-    case POSITION.AFTEREND:
-      container.after(element);
-      break;
-    case POSITION.BEFOREEND:
-      container.append(element);
-      break;
-  }
-};
-
-const remove = (component) => {
-  component.getElement().remove();
-  component.removeElement();
-};
-
-const toggleElement = (container, component, action) => {
-  const element = component.getElement();
-  switch (action) {
-    case `show`:
-      container.appendChild(element);
-      break;
-    case `hide`:
-      if (container.contains(element)) {
-        container.removeChild(element);
-      }
-      break;
-  }
-};
-
-const replace = (oldComponent, newComponent) => {
-  const parentElement = oldComponent.getElement().parentElement;
-  const newElement = newComponent.getElement();
-  const oldElement = oldComponent.getElement();
-
-  const isExistElements = !!(parentElement && newElement && oldElement);
-
-  if (isExistElements && parentElement.contains(oldElement)) {
-    parentElement.replaceChild(newElement, oldElement);
-  }
-};
-
-export {createElement, render, POSITION, remove, toggleElement, replace};
-
-//
 export const RenderPosition = {
   AFTERBEGIN: `afterbegin`,
   BEFOREEND: `beforeend`
@@ -81,11 +20,6 @@ export const render = (container, component, place = RenderPosition.BEFOREEND) =
       container.append(component.getElement());
       break;
   }
-};
-
-export const remove = (component) => {
-  component.getElement().remove();
-  component.removeElement();
 };
 
 export const removeChild = (childComponent) => {
