0d166c25a914417954c94fd316202bca243f94fd
j3n4r3v <jene4ka4reva@gmail.com>
2021-02-03 20:23:39 +0200

    fix esc bug

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ import UserProfileRatingPresenter from "./presenter/user-profile-rating-presente
 import {render} from "./utils/render-utils";
 import {NavigationItem} from "./const";
 
-const AUTHORIZATION = `Basic hhhesgsadgeawdawderhseg`;
+const AUTHORIZATION = `Basic hhheswdawderhseg`;
 const END_POINT = `https://13.ecmascript.pages.academy/cinemaddict`;
 
 const siteHeaderElement = document.querySelector(`.header`);
