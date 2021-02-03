9c6c2e14de1326c25b80f61783806a851ff23273
j3n4r3v <jene4ka4reva@gmail.com>
2021-02-03 19:43:24 +0200

    fix delete control

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ import UserProfileRatingPresenter from "./presenter/user-profile-rating-presente
 import {render} from "./utils/render-utils";
 import {NavigationItem} from "./const";
 
-const AUTHORIZATION = `Basic hhhrserhseg`;
+const AUTHORIZATION = `Basic hhhesgsadgeawdawderhseg`;
 const END_POINT = `https://13.ecmascript.pages.academy/cinemaddict`;
 
 const siteHeaderElement = document.querySelector(`.header`);
